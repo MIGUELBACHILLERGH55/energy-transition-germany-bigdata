049fbbb212559830f3304a0db4e596a68b004596
Tomás Morales Galván <trabajostomaslh@gmail.com>
2026-01-20 09:00:51 +0000

    feat(smard): support last available timeseries download

--- src/extract/sources/smard/run/timeseries.py
+++ src/extract/sources/smard/run/timeseries.py
@@ -3,14 +3,10 @@ from src.extract.sources.smard.extractors.timeseries_extractor import (
     SmardTimeseriesExtractor,
 )
 
-
 proj_config = config.project_config
 smard_source = config.sources["smard"]
 
 if __name__ == "__main__":
-    smard_timeseries_extractor = SmardTimeseriesExtractor(
-        proj_config,
-        smard_source,
-    )
+    smard_timeseries_extractor = SmardTimeseriesExtractor(proj_config, smard_source)
 
     smard_timeseries_extractor.run()
