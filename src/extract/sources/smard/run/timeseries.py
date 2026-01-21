c913aa0d892c81649fa68255881844d529ba6537
Tomás Morales Galván <trabajostomasmorales@gmail.com>
2026-01-21 21:30:12 +0000

    chore(smard): add verbose flag to timeseries run entrypoint

--- src/extract/sources/smard/run/timeseries.py
+++ src/extract/sources/smard/run/timeseries.py
@@ -3,10 +3,13 @@ from src.extract.sources.smard.extractors.timeseries_extractor import (
     SmardTimeseriesExtractor,
 )
 
+
 proj_config = config.project_config
 smard_source = config.sources["smard"]
 
 if __name__ == "__main__":
-    smard_timeseries_extractor = SmardTimeseriesExtractor(proj_config, smard_source)
+    smard_timeseries_extractor = SmardTimeseriesExtractor(
+        proj_config, smard_source, verbose=False
+    )
 
     smard_timeseries_extractor.run()
