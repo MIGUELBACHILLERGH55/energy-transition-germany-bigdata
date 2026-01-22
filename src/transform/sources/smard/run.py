f4a709ff6af898466d68fc59ddcc4ff90c9a182a
Tomás Morales Galván <trabajostomasmorales@gmail.com>
2026-01-22 10:43:16 +0000

    chore(transform-smard): rename source variable for clarity

--- src/transform/sources/smard/run.py
+++ src/transform/sources/smard/run.py
@@ -2,8 +2,8 @@ from src.transform.sources.smard.pipeline import SmardTimeseriesPipeline
 from src.config.loader import config, spark_session
 
 proj_config = config.project_config
-opsd_source = config.sources["smard"]
+smard_source = config.sources["smard"]
 
 if __name__ == "__main__":
-    smard_pipeline = SmardTimeseriesPipeline(spark_session, proj_config, opsd_source)
+    smard_pipeline = SmardTimeseriesPipeline(spark_session, proj_config, smard_source)
     smard_pipeline.run()
